e5414e2f677f49da90b5eca5fa55059ae274b0bd
Давлюд Анатолий Георгиевич <davlud_ag@bytechs.by>
2022-12-19 02:59:28 +0300

    refactor: cp-method

--- src/cmd/commands/cp.js
+++ src/cmd/commands/cp.js
@@ -1,18 +1,18 @@
 import { open } from 'node:fs/promises';
 import fs from 'node:fs/promises';
-import { createWriteStream  } from 'node:fs';
+import { createWriteStream } from 'node:fs';
 import dir from '../../directory/dir.js';
 import { pipeline } from 'node:stream/promises';
 
 const cpFile = async (pathToFile, pathToDirectory) => {
   try {
     const pathToFileOrigin = dir.resolvePath(pathToFile);
-    const pathToDestinationDir = dir.resolvePath(pathToDirectory, dir.basenamePath(pathToFile))
+    const pathToDestinationDir = dir.resolvePath(pathToDirectory, dir.basenamePath(pathToFile));
 
     const filehandle = await open(pathToFileOrigin, 'r', fs.constants.O_RDONLY);
     const fileOrigin = filehandle.createReadStream();
 
-    const fileDestination = createWriteStream(pathToDestinationDir, { flags: 'wx' })
+    const fileDestination = createWriteStream(pathToDestinationDir, { flags: 'wx' });
 
     await pipeline(
       fileOrigin,
